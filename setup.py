7a2aa1bcdeb374f4f698193713b23f520d328e1f
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-03-16 11:32:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 """This module sets up the package for distribution."""
 from setuptools import setup, find_packages
-
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 setup(
-    name='stockdatamanager',
+    name='automotifs',
     version='0.6',
     packages=find_packages(),
-    description='A comprehensive library for financial analysis',
+    description='A wrapper for automatic Motif Detection',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Giorgio Micaletto',
     author_email='giorgio.micaletto@studbocconi.it',
     url='https://github.com/GiorgioMB/auto_dotmotif/',
-    install_requires=[
-        'pandas>=1.1.5',
-        'yfinance>=0.1.63',
-        'requests>=2.25.1',
-        'pandas-datareader>= 0.10.0',
-        'numpy>=1.23'
-    ],
+    install_requires=requirements,
     python_requires='>=3.6',
 )
