edc76b832bb5b7d6926b1477cd22078542f3cbab
Giorgio <122452088+GiorgioMB@users.noreply.github.com>
2024-03-16 15:52:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name='automotifs',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     description='A wrapper for automatic Motif Detection',
     long_description=open('README.md').read(),
